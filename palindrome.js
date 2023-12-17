fab238be9b147595c3f43f9ee37c96570fdb9cfc
Umair <67766295+Muze1@users.noreply.github.com>
2023-12-17 19:34:58 +0000

    Update palindrome.js

--- palindrome.js
+++ palindrome.js
@@ -2,20 +2,34 @@
 
 const prompt = require('prompt-sync')();
 
-let word = prompt("Please enter a word: ");
-let lower = word.toLowerCase();
-let list = lower.split('');
-let reversedList = list.toReversed();
+function isPalindrome() {
+    let word = prompt("Please enter a word: ");
+    let lower = word.toLowerCase();
+    let list = lower.split('');
+    let loopComplete = true;
 
-for (let i = 0; i < (list.length / 2); i++) {
-    let firstElement = list[i];
-    let lastElement = list[list.length - 1 - i];
+    for (let i = 0; i < (list.length / 2); i++) {
+        let firstElement = list[i];
+        let lastElement = list[list.length - 1 - i];
 
-    if (firstElement === lastElement) {
-        continue;
-    } else {
-        console.log(`The word "${word}" is not a palindrome.`) // Self-reminder: Used backticks here instead of double or single quotes. This allows the use of template literals.
+        if (firstElement !== lastElement) {
+            console.log(`No, the word "${word}" is not a palindrome.`) // Self-reminder: Used backticks here instead of double or single quotes. This allows the use of template literals.
+            loopComplete = false;
+            break;
+        }
+    }
+
+    if (loopComplete) {
+        console.log(`Yes, the word ${word} is a palindrome.`)
+    }
+}
+
+while (true) {
+    isPalindrome();
+
+    let repeat = prompt("Would you like to enter another word? (yes/no): ")
+    if (repeat.toLowerCase !== 'yes') {
+        console.log("Bye... :(")
         break;
     }
 }
-console.log(`The word ${word} is a palindrome.`)
