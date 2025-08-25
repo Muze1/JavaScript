2533c308cec1470c92041d1a72db49df688b50ec
Muze1 <umair.mehmood15@gmail.com>
2025-08-25 14:58:53 +0100

    Play with variables and start exercises

--- odin_project_prac/javascript.js
+++ odin_project_prac/javascript.js
@@ -1,12 +1,28 @@
-let name = "John";
-let surname = "Doe";
+// // Variables
+// // let
+// let name = "John";
+// let surname = "Doe";
 
-console.log(name);
-console.log(surname);
+// console.log(name);
+// console.log(surname);
 
-let age = 11;
-console.log(age);
+// let age = 11;
+// console.log(age);
 
-age = 54;
+// age = 54;
 
-console.log(age);
+// console.log(age);
+
+// // const
+// const pi = 3.14;
+// pi = 10;
+
+// // console.log(pi); // Will give an error!
+
+// // 'var' also exists, and you may come across it, but it used to be the old way to declare a variable
+// // It's now replaced by const and let
+
+// Assignment exercises
+
+// 1)
+console.log(23 + 97);
