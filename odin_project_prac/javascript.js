ce33fefbed14f0c164b0ef8704846041daabd38b
Muze1 <umair.mehmood15@gmail.com>
2025-08-26 17:08:06 +0100

    Add to file

--- odin_project_prac/javascript.js
+++ odin_project_prac/javascript.js
@@ -26,3 +26,22 @@
 
 // 1)
 console.log(23 + 97);
+
+// 2)
+console.log(23 + 97 + 84 + 29 + 17 + 65);
+
+// 3)
+console.log((4 + 6 + 9) / 77);
+
+// 4)
+let a = 10;
+console.log(a);
+a = 7;
+let b = 7 * a;
+console.log(b);
+
+// 5)
+const max = 57;
+const actual = max - 13;
+const percentage = actual / max;
+console.log(percentage);
