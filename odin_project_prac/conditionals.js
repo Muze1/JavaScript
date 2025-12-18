3597952364d5400c9e076ddb3380670b39971521
Muze1 <umair.mehmood15@gmail.com>
2025-12-18 15:41:02 +0000

    Add conditional exercise

--- odin_project_prac/conditionals.js
+++ odin_project_prac/conditionals.js
@@ -5,3 +5,12 @@ if (x == 0) {
 } else {
     console.log("Not equal to zero.")
 }
+
+let userInput = "ECMAScript";
+
+console.log('What is the "official" name of JavaScript?')
+if (userInput === "ECMAScript") {
+    console.log("Right!")
+} else {
+    console.log(`You don't know? "ECMAScript!`)
+}
