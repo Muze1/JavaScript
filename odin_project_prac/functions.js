6983824ee081ef89f97ef569375a943fbb08d754
Muze1 <umair.mehmood15@gmail.com>
2026-01-06 10:01:21 +0000

    Practice functions

---
+++ odin_project_prac/functions.js
@@ -0,0 +1,13 @@
+let add7 = c => c + 7;
+
+console.log(add7(10));
+
+// let multiply = function multiply (num1, num2) {
+//     return num1 * num2;
+// }
+
+// console.log(multiply(3, 2));
+
+let multiply = (a, b) => a * b;
+
+console.log(multiply(3, 2));
