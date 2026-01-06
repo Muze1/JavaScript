55d44dd443e12fe98ae6436193abe02af0eb00b5
Muze1 <umair.mehmood15@gmail.com>
2026-01-06 10:45:12 +0000

    Practice writing functions

--- odin_project_prac/functions.js
+++ odin_project_prac/functions.js
@@ -2,11 +2,13 @@ let add7 = c => c + 7;
 
 console.log(add7(10));
 
+
 // let multiply = function multiply (num1, num2) {
 //     return num1 * num2;
 // }
 
 // console.log(multiply(3, 2));
+
 
 let multiply = (a, b) => a * b;
 
@@ -23,4 +25,12 @@ let capFirst = function(string) {
     return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
 }
 
-console.log(capFirst("HELLooo"));
+console.log(capFirst("abcd"));
+console.log(capFirst("ABCD"));
+console.log(capFirst("aBcD"));
+
+
+let lastLetter = function(string) {
+    return string.charAt(string.length-1);
+}
+console.log(lastLetter("abcd"));
