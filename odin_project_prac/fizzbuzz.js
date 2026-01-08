dbfe724a96e73c2681fecaf10be3b8511ba0cac8
Muze1 <umair.mehmood15@gmail.com>
2026-01-08 18:25:28 +0000

    Add first conditional for "Fizz"

--- odin_project_prac/fizzbuzz.js
+++ odin_project_prac/fizzbuzz.js
@@ -1,6 +1,14 @@
 let answer = parseInt(prompt("Choose a number to FizzBuzz up to: "))
 
 for (let i = 1; i <= answer; i++) {
-    console.log();
+    console.log(i);
+
+    if (i % 3 === 0) {
+        console.log("Fizz");
+    } else {
+        console.log(i);
+    }
+
+    
 }
 
