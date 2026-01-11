6350000455e003feda543b95ff3e1ec006b95fa2
Muze1 <umair.mehmood15@gmail.com>
2026-01-11 22:13:26 +0000

    Create function to play one round

--- odin_project_prac/rock-paper-scissors/main.js
+++ odin_project_prac/rock-paper-scissors/main.js
@@ -3,20 +3,43 @@ function getComputerChoice() {
     let generateNumber = Math.floor(Math.random() * 3);
 
     if (generateNumber === 2) {
-        return "Scissors"
+        return "scissors"
     } else if (generateNumber === 1) {
-        return "Paper"
+        return "paper"
     } else {
-        return "Rock"
+        return "rock"
     }
 }
 
 console.log(getComputerChoice());
 
 function getHumanChoice() {
-    let humanChoice = prompt("Enter rock, paper or scissors: ")
+    let human = prompt("Enter rock, paper or scissors: ")
 
-    return humanChoice;
+    return human;
 }
 
 console.log(getHumanChoice());
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+
+    if (humanChoice === computerChoice) {
+        return "Draw! No points";
+    } else if (((humanChoice === "rock") && (computerChoice === "paper")) || 
+    ((humanChoice === "paper") && (computerChoice === "scissors")) ||
+    ((humanChoice === "scissors") && (computerChoice === "rock"))) {
+
+        computerScore++;
+        return `You lose! ${computerChoice} beats ${humanChoice}`;
+    } else {
+
+        humanScore++;
+        return `You win! ${humanChoice} beats ${computerChoice}`;
+    }
+}
+
+console.log(playRound(getHumanChoice(),getComputerChoice()));
