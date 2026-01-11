25834217518f7e0b394bdd2a5df77f59ddebd779
Muze1 <umair.mehmood15@gmail.com>
2026-01-11 21:39:15 +0000

    Create function for computer choice

--- odin_project_prac/rock-paper-scissors/main.js
+++ odin_project_prac/rock-paper-scissors/main.js
@@ -1,1 +1,14 @@
-console.log("Hello, world")
+function getComputerChoice() {
+    // Generate a random number which can be either 0, 1 or 2
+    let generateNumber = Math.floor(Math.random() * 3);
+
+    if (generateNumber === 2) {
+        return "Scissors"
+    } else if (generateNumber === 1) {
+        return "Paper"
+    } else {
+        return "Rock"
+    }
+}
+
+console.log(getComputerChoice());
