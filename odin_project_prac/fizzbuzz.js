a54f23c9020cf7d3f56d8873e01f955fc53290ae
Muze1 <umair.mehmood15@gmail.com>
2026-01-11 20:29:38 +0000

    Add final changes

--- odin_project_prac/fizzbuzz.js
+++ odin_project_prac/fizzbuzz.js
@@ -1,14 +1,12 @@
-let answer = parseInt(prompt("Choose a number to FizzBuzz up to: "))
+let answer = parseInt(prompt("Choose a number to FizzBuzz up to: "));
 
 for (let i = 1; i <= answer; i++) {
-    console.log(i);
-
     if (i % 3 === 0) {
         console.log("Fizz");
     } else {
         console.log(i);
     }
 
-    
+
 }
 
