4afe480f0c9baa5e1f66b1a37c2b4789c0d802e5
Umair <umair.mehmood15@gmail.com>
2026-04-05 16:06:15 +0100

    Add final elements

--- DOM-practice/script.js
+++ DOM-practice/script.js
@@ -6,9 +6,31 @@ content.textContent = "This is going to be awesome!";
 
 container.appendChild(content);
 
+// Add red text
 const text = document.createElement("text");
 text.classList.add("red-text");
 text.style.color = "red";
 text.textContent = "Hey, I'm red!";
 
 container.appendChild(text);
+
+// Add blue h3
+const header = document.createElement("h3");
+header.classList.add("blue-heading");
+header.textContent = "I'm a blue h3!"
+header.style.color = "blue";
+container.appendChild(header)
+
+// Add div with black border and pink background
+const divBox = document.createElement("div");
+divBox.classList.add("box-div");
+divBox.style.border = "2px solid black";
+divBox.style.backgroundColor = "pink";
+
+const divHeader = document.createElement("h1");
+divHeader.textContent = "I'm in a div";
+const divText = document.createElement("p");
+divText.textContent = "ME TOO!";
+divBox.appendChild(divHeader)
+divBox.appendChild(divText)
+container.appendChild(divBox)
