b3800e765067815290f2edd4fc898c72e26fd5bb
Umair <umair.mehmood15@gmail.com>
2026-04-05 14:47:50 +0100

    Correct syntax

--- DOM-practice/script.js
+++ DOM-practice/script.js
@@ -5,3 +5,10 @@ content.classList.add("content");
 content.textContent = "This is going to be awesome!";
 
 container.appendChild(content);
+
+const text = document.createElement("text");
+text.classList.add("red-text");
+text.style.color = "red";
+text.textContent = "Hey, I'm red!";
+
+container.appendChild(text);
